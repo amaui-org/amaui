8c74c9f7fdfc8768a8af71e45e4906eacedb0d5f
Lazar <lazareric.com@gmail.com>
2022-09-18 03:25:44 +0200

    m

--- packages/amaui-ui-react/src/Image/Image.tsx
+++ packages/amaui-ui-react/src/Image/Image.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { classNames, style, useAmauiTheme } from '@amaui/style-react';
 
+import Type from '../Type';
 import Surface from '../Surface';
 
 import { staticClassName } from '../utils';
-import Type from '../Type';
 
 const useStyle = style(theme => ({
   root: {
