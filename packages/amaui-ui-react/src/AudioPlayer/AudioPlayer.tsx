34ed2908cbefdbe68402aaf9bca6e08479f40357
Lazar Eric <lazareric.com@gmail.com>
2023-10-17 19:35:48 +0200

    m

--- packages/amaui-ui-react/src/AudioPlayer/AudioPlayer.tsx
+++ packages/amaui-ui-react/src/AudioPlayer/AudioPlayer.tsx
@@ -188,18 +188,78 @@ const IconMaterialVolumeOffRounded = React.forwardRef((props: any, ref) => {
   );
 });
 
-const IconMaterialHighQualityRounded = React.forwardRef((props: any, ref) => {
+const IconMaterialSettingsRounded = React.forwardRef((props: any, ref) => {
 
   return (
     <Icon
       ref={ref}
 
-      name='HighQualityRounded'
-      short_name='HighQuality'
+      name='SettingsRounded'
+
+      short_name='Settings'
+
+      viewBox='0 0 24 24'
 
       {...props}
     >
-      <path d="M6.75 15Q7.075 15 7.287 14.787Q7.5 14.575 7.5 14.25V13H9.5V14.25Q9.5 14.575 9.713 14.787Q9.925 15 10.25 15Q10.575 15 10.788 14.787Q11 14.575 11 14.25V9.75Q11 9.425 10.788 9.212Q10.575 9 10.25 9Q9.925 9 9.713 9.212Q9.5 9.425 9.5 9.75V11.5H7.5V9.75Q7.5 9.425 7.287 9.212Q7.075 9 6.75 9Q6.425 9 6.213 9.212Q6 9.425 6 9.75V14.25Q6 14.575 6.213 14.787Q6.425 15 6.75 15ZM15.5 16.5Q15.825 16.5 16.038 16.288Q16.25 16.075 16.25 15.75V15H17Q17.425 15 17.712 14.712Q18 14.425 18 14V10Q18 9.575 17.712 9.287Q17.425 9 17 9H14Q13.575 9 13.288 9.287Q13 9.575 13 10V14Q13 14.425 13.288 14.712Q13.575 15 14 15H14.75V15.75Q14.75 16.075 14.963 16.288Q15.175 16.5 15.5 16.5ZM14.5 13.5Q14.5 13.5 14.5 13.5Q14.5 13.5 14.5 13.5V10.5Q14.5 10.5 14.5 10.5Q14.5 10.5 14.5 10.5H16.5Q16.5 10.5 16.5 10.5Q16.5 10.5 16.5 10.5V13.5Q16.5 13.5 16.5 13.5Q16.5 13.5 16.5 13.5ZM4 20Q3.175 20 2.588 19.413Q2 18.825 2 18V6Q2 5.175 2.588 4.588Q3.175 4 4 4H20Q20.825 4 21.413 4.588Q22 5.175 22 6V18Q22 18.825 21.413 19.413Q20.825 20 20 20ZM4 18Q4 18 4 18Q4 18 4 18V6Q4 6 4 6Q4 6 4 6Q4 6 4 6Q4 6 4 6V18Q4 18 4 18Q4 18 4 18ZM4 18H20Q20 18 20 18Q20 18 20 18V6Q20 6 20 6Q20 6 20 6H4Q4 6 4 6Q4 6 4 6V18Q4 18 4 18Q4 18 4 18Z" />
+      <path d="M13.875 22h-3.75q-.375 0-.65-.25t-.325-.625l-.3-2.325q-.325-.125-.612-.3-.288-.175-.563-.375l-2.175.9q-.35.125-.7.025t-.55-.425L2.4 15.4q-.2-.325-.125-.7.075-.375.375-.6l1.875-1.425Q4.5 12.5 4.5 12.337v-.675q0-.162.025-.337L2.65 9.9q-.3-.225-.375-.6-.075-.375.125-.7l1.85-3.225q.175-.35.537-.438.363-.087.713.038l2.175.9q.275-.2.575-.375.3-.175.6-.3l.3-2.325q.05-.375.325-.625t.65-.25h3.75q.375 0 .65.25t.325.625l.3 2.325q.325.125.613.3.287.175.562.375l2.175-.9q.35-.125.7-.025t.55.425L21.6 8.6q.2.325.125.7-.075.375-.375.6l-1.875 1.425q.025.175.025.337v.675q0 .163-.05.338l1.875 1.425q.3.225.375.6.075.375-.125.7l-1.85 3.2q-.2.325-.562.438-.363.112-.713-.013l-2.125-.9q-.275.2-.575.375-.3.175-.6.3l-.3 2.325q-.05.375-.325.625t-.65.25Zm-1.825-6.5q1.45 0 2.475-1.025Q15.55 13.45 15.55 12q0-1.45-1.025-2.475Q13.5 8.5 12.05 8.5q-1.475 0-2.488 1.025Q8.55 10.55 8.55 12q0 1.45 1.012 2.475Q10.575 15.5 12.05 15.5Zm0-2q-.625 0-1.062-.438-.438-.437-.438-1.062t.438-1.062q.437-.438 1.062-.438t1.063.438q.437.437.437 1.062t-.437 1.062q-.438.438-1.063.438ZM12 12Zm-1 8h1.975l.35-2.65q.775-.2 1.438-.588.662-.387 1.212-.937l2.475 1.025.975-1.7-2.15-1.625q.125-.35.175-.738.05-.387.05-.787t-.05-.788q-.05-.387-.175-.737l2.15-1.625-.975-1.7-2.475 1.05q-.55-.575-1.212-.963-.663-.387-1.438-.587L13 4h-1.975l-.35 2.65q-.775.2-1.437.587-.663.388-1.213.938L5.55 7.15l-.975 1.7 2.15 1.6q-.125.375-.175.75-.05.375-.05.8 0 .4.05.775t.175.75l-2.15 1.625.975 1.7 2.475-1.05q.55.575 1.213.962.662.388 1.437.588Z" />
+    </Icon>
+  );
+});
+
+const IconMaterialTuneRounded = React.forwardRef((props: any, ref) => {
+
+  return (
+    <Icon
+      ref={ref}
+
+      name='TuneRounded'
+
+      short_name='Tune'
+
+      viewBox='0 0 24 24'
+
+      {...props}
+    >
+      <path d="M4 19q-.425 0-.712-.288Q3 18.425 3 18t.288-.712Q3.575 17 4 17h4q.425 0 .713.288Q9 17.575 9 18t-.287.712Q8.425 19 8 19ZM4 7q-.425 0-.712-.287Q3 6.425 3 6t.288-.713Q3.575 5 4 5h8q.425 0 .713.287Q13 5.575 13 6t-.287.713Q12.425 7 12 7Zm8 14q-.425 0-.712-.288Q11 20.425 11 20v-4q0-.425.288-.713Q11.575 15 12 15t.713.287Q13 15.575 13 16v1h7q.425 0 .712.288.288.287.288.712t-.288.712Q20.425 19 20 19h-7v1q0 .425-.287.712Q12.425 21 12 21Zm-4-6q-.425 0-.713-.288Q7 14.425 7 14v-1H4q-.425 0-.712-.288Q3 12.425 3 12t.288-.713Q3.575 11 4 11h3v-1q0-.425.287-.713Q7.575 9 8 9t.713.287Q9 9.575 9 10v4q0 .425-.287.712Q8.425 15 8 15Zm4-2q-.425 0-.712-.288Q11 12.425 11 12t.288-.713Q11.575 11 12 11h8q.425 0 .712.287.288.288.288.713t-.288.712Q20.425 13 20 13Zm4-4q-.425 0-.712-.288Q15 8.425 15 8V4q0-.425.288-.713Q15.575 3 16 3t.712.287Q17 3.575 17 4v1h3q.425 0 .712.287Q21 5.575 21 6t-.288.713Q20.425 7 20 7h-3v1q0 .425-.288.712Q16.425 9 16 9Z" />
+    </Icon>
+  );
+});
+
+const IconMaterialSlowMotionVideoRounded = React.forwardRef((props: any, ref) => {
+
+  return (
+    <Icon
+      ref={ref}
+
+      name='SlowMotionVideoRounded'
+
+      short_name='SlowMotionVideo'
+
+      viewBox='0 0 24 24'
+
+      {...props}
+    >
+      <path d="M11.025 15.5q-.5.325-1.025.05-.525-.275-.525-.875v-5.35q0-.6.525-.875t1.025.05l4.15 2.65q.225.15.337.375.113.225.113.475 0 .25-.113.475-.112.225-.337.375Zm-1.2 6.275q-.875-.2-1.687-.525-.813-.325-1.588-.825-.4-.25-.437-.7-.038-.45.312-.8.25-.25.637-.287.388-.038.713.162.55.35 1.138.587.587.238 1.212.388.35.1.588.388.237.287.237.637 0 .5-.337.787-.338.288-.788.188ZM5 17.5q-.35.35-.787.312-.438-.037-.688-.412-.5-.725-.837-1.537-.338-.813-.538-1.713-.125-.45.162-.8Q2.6 13 3.1 13q.35 0 .65.25.3.25.4.6.15.65.4 1.225.25.575.575 1.125.2.3.162.675-.037.375-.287.625ZM3.125 11q-.5 0-.8-.35-.3-.35-.2-.8.2-.875.55-1.713.35-.837.85-1.562.25-.375.688-.4Q4.65 6.15 5 6.5q.25.25.287.625.038.375-.162.675-.325.525-.575 1.112-.25.588-.4 1.238-.1.35-.387.6-.288.25-.638.25ZM14.05 21.75q-.45.1-.775-.175t-.325-.75q0-.35.238-.65.237-.3.587-.4 2.675-.7 4.4-2.837Q19.9 14.8 19.9 12t-1.725-4.975Q16.45 4.85 13.775 4.2q-.35-.1-.587-.388-.238-.287-.238-.637 0-.475.338-.763.337-.287.787-.187Q17.5 2.975 19.7 5.7t2.2 6.3q0 3.55-2.212 6.25-2.213 2.7-5.638 3.5ZM7.7 5.225q-.325.2-.687.15-.363-.05-.613-.3-.35-.35-.312-.8.037-.45.437-.7.775-.5 1.6-.825.825-.325 1.7-.525.45-.1.788.187.337.288.337.763 0 .35-.25.637-.25.288-.6.388-.625.15-1.225.412-.6.263-1.175.613Z" />
+    </Icon>
+  );
+});
+
+const IconMaterialArrowBackIosRounded = React.forwardRef((props: any, ref) => {
+
+  return (
+    <Icon
+      ref={ref}
+
+      name='ArrowBackIosRounded'
+
+      short_name='ArrowBackIos'
+
+      viewBox='0 0 24 24'
+
+      {...props}
+    >
+      <path d="M9.125 21.1.7 12.7q-.15-.15-.212-.325Q.425 12.2.425 12t.063-.375Q.55 11.45.7 11.3l8.425-8.425q.35-.35.875-.35t.9.375q.375.375.375.875t-.375.875L3.55 12l7.35 7.35q.35.35.35.862 0 .513-.375.888t-.875.375q-.5 0-.875-.375Z" />
     </Icon>
   );
 });
@@ -227,7 +287,9 @@ export interface IAudioPlayer extends IBaseElement {
 
   forward?: boolean;
   backward?: boolean;
+  settings?: boolean;
   quality?: boolean;
+  playbackSpeed?: boolean;
 
   startMediaSessionOnPlay?: boolean;
 
@@ -239,7 +301,10 @@ export interface IAudioPlayer extends IBaseElement {
   IconBackward?: TElementReference;
   IconVolume?: TElementReference;
   IconVolumeMuted?: TElementReference;
+  IconBack?: TElementReference;
+  IconSettings?: TElementReference;
   IconQuality?: TElementReference;
+  IconPlaybackSpeed?: TElementReference;
 
   PlayButtonProps?: TPropsAny;
   ForwardButtonProps?: TPropsAny;
@@ -250,8 +315,9 @@ export interface IAudioPlayer extends IBaseElement {
   TimelineProps?: TPropsAny;
   VolumeProps?: TPropsAny;
   SliderProps?: TPropsAny;
+  SettingsButtonProps?: TPropsAny;
+  SettingsMenuProps?: TPropsAny;
   QualityButtonProps?: TPropsAny;
-  QualityMenuProps?: TPropsAny;
 }
 
 const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
@@ -283,7 +349,9 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
 
     forward,
     backward,
+    settings = true,
     quality: quality_ = true,
+    playbackSpeed: playbackSpeed_ = true,
 
     startMediaSessionOnPlay,
 
@@ -295,7 +363,10 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
     IconBackward = IconMaterialForwardMediaRounded,
     IconVolume = IconMaterialVolumeDownAltRounded,
     IconVolumeMuted = IconMaterialVolumeOffRounded,
-    IconQuality = IconMaterialHighQualityRounded,
+    IconBack = IconMaterialArrowBackIosRounded,
+    IconSettings = IconMaterialSettingsRounded,
+    IconQuality = IconMaterialTuneRounded,
+    IconPlaybackSpeed = IconMaterialSlowMotionVideoRounded,
 
     PlayButtonProps,
     ForwardButtonProps,
@@ -306,8 +377,9 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
     TimelineProps,
     VolumeProps,
     SliderProps,
+    SettingsButtonProps,
+    SettingsMenuProps,
     QualityButtonProps,
-    QualityMenuProps,
 
     className,
 
@@ -327,6 +399,8 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
   const [volumeVisible, setVolumeVisible] = React.useState(false);
   const [updating, setUpdating] = React.useState<any>(false);
   const [quality, setQuality] = React.useState<any>();
+  const [playbackSpeed, setPlaybackSpeed] = React.useState(1);
+  const [openMenu, setOpenMenu] = React.useState<any>();
 
   const refs = {
     root: React.useRef<any>(),
@@ -414,6 +488,16 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
 
     // update MediaSession
     refs.updateMediaSession.current();
+  }, []);
+
+  const onPlaybackSpeed = React.useCallback((value_: number) => {
+    const value = clamp(value_, 0, 2);
+
+    setPlaybackSpeed(value);
+
+    setOpenMenu(null);
+
+    refs.audio.current!.playbackRate = value;
   }, []);
 
   const onForward = React.useCallback((details?: MediaSessionActionDetails) => {
@@ -523,6 +607,12 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
       }
     });
 
+    audio.addEventListener('ratechange', () => {
+      const value = audio.playbackRate;
+
+      setPlaybackSpeed(value);
+    });
+
     // start MediaSession
     startMediaSession();
 
@@ -622,58 +712,190 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
 
   const onQuality = React.useCallback((version: any) => {
     setQuality(version);
+
+    setOpenMenu(null);
   }, []);
 
-  const getQualityMenuItems = () => {
+  const onSettingsMenuClose = React.useCallback(() => {
+    setOpenMenu(null);
+  }, []);
+
+  const getSettingsMenuItems = () => {
     const itemProps = {
-      menuCloseOnClick: true,
       button: true,
-      size: 'small'
+      startAlign: 'center',
+      endAlign: 'center',
+      size: 'small',
+      className: 'amaui-videoPlayer-option'
     };
 
-    const items = [
-      <ListItem
-        key='original'
+    const items: any = [];
 
-        primary={(
-          <Type
-            version='b3'
-          >
-            original
-          </Type>
-        )}
+    if (openMenu) {
+      items.push(
+        <ListItem
+          key='back'
 
-        onClick={() => quality ? onQuality(null) : undefined}
+          start={(
+            <IconBack
+              size='very small'
+            />
+          )}
 
-        selected={!quality}
+          primary={(
+            <Type
+              version='b3'
+            >
+              Back
+            </Type>
+          )}
 
-        {...itemProps}
-      />
-    ];
+          onClick={() => setOpenMenu(null)}
 
-    // versions?.forEach((version: any, index: number) => {
-    //   const isSelected = quality?.id === version?.id;
+          {...itemProps}
+        />
+      );
+    }
 
-    //   items.push(
-    //     <ListItem
-    //       key={index}
+    if (!openMenu) {
+      if (playbackSpeed_) items.push(
+        <ListItem
+          key='playbackSpeed'
 
-    //       primary={(
-    //         <Type
-    //           version='b3'
-    //         >
-    //           {version?.meta?.resolution}p
-    //         </Type>
-    //       )}
+          start={(
+            <IconPlaybackSpeed
+              size='small'
+            />
+          )}
 
-    //       onClick={() => !isSelected ? onQuality(version) : undefined}
+          primary={(
+            <Type
+              version='b3'
+            >
+              Playback speed
+            </Type>
+          )}
 
-    //       selected={isSelected}
+          end={(
+            <Type
+              version='b3'
 
-    //       {...itemProps}
-    //     />
-    //   );
-    // });
+              priority='secondary'
+            >
+              {playbackSpeed === 1 ? 'Normal' : playbackSpeed}
+            </Type>
+          )}
+
+          onClick={() => setOpenMenu('playbackSpeed')}
+
+          {...itemProps}
+        />
+      );
+
+      if (quality_) items.push(
+        <ListItem
+          key='quality'
+
+          start={(
+            <IconQuality
+              size='small'
+            />
+          )}
+
+          primary={(
+            <Type
+              version='b3'
+            >
+              Quality
+            </Type>
+          )}
+
+          end={(
+            <Type
+              version='b3'
+
+              priority='secondary'
+            >
+              {!quality ? 'Original' : `${quality?.meta?.resolution}p`}
+            </Type>
+          )}
+
+          onClick={() => setOpenMenu('quality')}
+
+          {...itemProps}
+        />
+      );
+    }
+    else if (openMenu === 'quality') {
+      items.push(
+        <ListItem
+          key='original'
+
+          primary={(
+            <Type
+              version='b3'
+            >
+              Original
+            </Type>
+          )}
+
+          onClick={() => quality ? onQuality(null) : undefined}
+
+          selected={!quality}
+
+          {...itemProps}
+        />
+      );
+
+      // versions?.forEach((version: any, index: number) => {
+      //   const isSelected = quality?.id === version?.id;
+
+      //   items.push(
+      //     <ListItem
+      //       key={index}
+
+      //       primary={(
+      //         <Type
+      //           version='b3'
+      //         >
+      //           {version?.meta?.resolution}p
+      //         </Type>
+      //       )}
+
+      //       onClick={() => !isSelected ? onQuality(version) : undefined}
+
+      //       selected={isSelected}
+
+      //       {...itemProps}
+      //     />
+      //   );
+      // });
+    }
+    else if (openMenu === 'playbackSpeed') {
+      const options = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
+
+      options.forEach(option => {
+        items.push(
+          <ListItem
+            key={option}
+
+            primary={(
+              <Type
+                version='b3'
+              >
+                {option === 1 ? 'Normal' : option}
+              </Type>
+            )}
+
+            onClick={() => onPlaybackSpeed(option)}
+
+            selected={playbackSpeed === option}
+
+            {...itemProps}
+          />
+        );
+      });
+    }
 
     return items;
   };
@@ -980,9 +1202,9 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
               >
                 {startButtonsEnd}
 
-                {quality_ && (
+                {settings && (
                   <Menu
-                    menuItems={getQualityMenuItems()}
+                    menuItems={getSettingsMenuItems()}
 
                     position='top'
 
@@ -990,18 +1212,29 @@ const AudioPlayer = React.forwardRef((props_: IAudioPlayer, ref: any) => {
 
                     portal={false}
 
+                    onClose={onSettingsMenuClose}
+
+                    className='amaui-videoPlayer'
+
+                    includeParentQueries={['.amaui-videoPlayer']}
+
+                    {...SettingsMenuProps}
+
                     ListProps={{
+                      className: classNames([
+                        SettingsMenuProps?.ListProps?.className,
+                        classes.menuSettings
+                      ]),
+
                       size: 'small'
                     }}
-
-                    {...QualityMenuProps}
                   >
                     <IconButton
                       {...iconButtonProps}
 
-                      {...QualityButtonProps}
+                      {...SettingsButtonProps}
                     >
-                      <IconQuality />
+                      <IconSettings />
                     </IconButton>
                   </Menu>
                 )}
