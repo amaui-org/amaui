f74f5f63fa3d4c14266b5d6f197fcb2bc5608d0d
Lazar Eric <lazareric.com@gmail.com>
2023-10-28 18:28:14 +0200

    m

--- packages/amaui-ui-react/src/Avatar/Avatar.tsx
+++ packages/amaui-ui-react/src/Avatar/Avatar.tsx
@@ -24,7 +24,9 @@ const useStyle = styleMethod(theme => ({
   imageWrapper: {
     display: 'inline-flex',
     height: '100%',
-    width: '100%'
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 
   image: {
